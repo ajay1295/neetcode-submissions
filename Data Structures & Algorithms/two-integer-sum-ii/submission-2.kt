6cd402fb517d01ccbf6802ea305858a34bb139f1
ajay1295 <32745004+ajay1295@users.noreply.github.com>
2026-04-24 19:58:08 +0530

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
+        var left = 0
+        var right = numbers.size-1
+
+        while(left < right){
+            if(numbers[left] + numbers[right] > target){
+                right--
+            }else if(numbers[left] + numbers[right] < target){
+                left++
+            }else{
+                break
+            }
+        }
+        return intArrayOf(left+1,right+1)
+    }
+}
